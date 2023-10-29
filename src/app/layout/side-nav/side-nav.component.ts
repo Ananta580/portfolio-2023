58f3bc562e48e531661871c8f6d0ef5cf913c8d2
Ananta Poudel <anantapoudel580@gmail.com>
2023-10-29 12:35:33 +0545

    Refactored side menu code

--- src/app/layout/side-nav/side-nav.component.ts
+++ src/app/layout/side-nav/side-nav.component.ts
@@ -3,8 +3,43 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'p-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent {
+  sideMenus = [
+    { label: 'Welcome', icon: 'home', value: 'home' },
+    { label: 'Skills', icon: 'videocam', value: 'skill' },
+    { label: 'Experience', icon: 'work_history', value: 'experience' },
+  ];
 
+  professionalAccounts = [
+    {
+      label: 'Github',
+      image: '../../../assets/github.svg',
+      url: 'https://github.com/ananta580',
+    },
+    {
+      label: 'Upwork',
+      image: '../../../assets/upwork.svg',
+      url: 'https://www.upwork.com/freelancers/~0188bd9446a88c1e33',
+    },
+    {
+      label: 'Behance',
+      image: '../../../assets/behance.svg',
+      url: 'https://www.behance.net/anantapoudel',
+    },
+  ];
+
+  socialAccounts = [
+    {
+      label: 'Linkedin',
+      image: '../../../assets/linkedin.svg',
+      url: 'https://www.linkedin.com/in/ananta580/',
+    },
+    {
+      label: 'Youtube',
+      image: '../../../assets/youtube.svg',
+      url: 'https://www.youtube.com/@ananta.poudel',
+    },
+  ];
 }
