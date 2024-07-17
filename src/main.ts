16d1c78588647304a07e0ec6aec3b54490886799
Ananta Poudel <anantapoudel580@gmail.com>
2024-07-17 07:51:17 +0545

    intro console

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(() => {
+    console.info('-------------------------------------------------');
+    console.info('|       Hey, thanks for visting here! 🥰 🥰      |');
+    console.info('-------------------------------------------------');
+  })
+  .catch((err) => console.error(err));
