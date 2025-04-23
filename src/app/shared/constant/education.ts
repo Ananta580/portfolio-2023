2e97cd26e3e2eafbbfeb6693a30e29e48152de34
Ananta Poudel <anantapoudel580@gmail.com>
2025-04-23 13:01:49 -0400

    Did Ui required changes

--- src/app/shared/constant/education.ts
+++ src/app/shared/constant/education.ts
@@ -1,16 +1,20 @@
 export const EDUCATION = [
   {
-    degree: 'Post-graduate Diploma',
-    course: 'Full Stack Software Development',
+    degree: 'Postgraduate Diploma',
+    course: 'Software Development',
     year: '2023 - 2025',
-    image: '../../../../assets/images/lambton.jpg',
+    image:
+      'https://media.licdn.com/dms/image/v2/D560BAQG4MWYO9WC-5g/company-logo_200_200/company-logo_200_200/0/1666810801136/lambton_college_sarnia_logo?e=1750896000&v=beta&t=NWOqMVm0zovFlAdy_NahSOXhN2fvMbxfhAbrlT_d-Wk',
     college: 'Lambton College in Toronto',
+    collegeWebsite: 'https://www.lambtoncollege.ca/',
   },
   {
-    degree: "Bachelor's Of Engineering",
+    degree: 'Bachelor Of Engineering - BE',
     course: 'Computer Engineering',
     year: '2016 - 2020',
-    image: '../../../../assets/images/tu.png',
-    college: 'Tribhuwan University',
+    image:
+      'https://media.licdn.com/dms/image/v2/C560BAQG2ingZrQS1Dg/company-logo_200_200/company-logo_200_200/0/1676120000190/kantipur_engg_college_logo?e=1750896000&v=beta&t=Rwv4LfbayubTE2SAtZABZx6Lk2hD9krLBYC5ifD3cv0',
+    college: 'Kantipur Engineering College',
+    collegeWebsite: 'https://www.kec.edu.np/',
   },
 ];
--- src/app/shared/constant/experience.ts
+++ src/app/shared/constant/experience.ts
@@ -1,89 +1,137 @@
 export const WORK_EXPEREICNE = [
   {
     company: 'Beacontree Technologies',
-    companyWebsite: 'https://beacontree.ca/',
+    companyWebsite: 'https://beacontree.net/',
     companyLogo:
       'https://beacontree.net/wp-content/uploads/2022/02/beacontree-technologies.png',
-    position: 'Senior Software Engineer',
-    jobType: 'Full Time',
-    jobLocation: 'Canada, Remote',
-    timeline: 'Apr 2025 - Present',
-    workDescription: [
-      'Transitioning from co-op to full-time Senior Software Engineer.',
-      'Leading full-stack development with React, Node.js, and Android.',
-      'Architecting scalable solutions for client-facing applications.',
-      'Driving technical excellence and mentoring junior developers.',
+    jobLocation: 'Canada, Hybrid',
+    positions: [
+      {
+        position: 'Lead Software Engineer',
+        jobType: 'Full Time',
+        timeline: 'Apr 2025 - Present',
+        skills: ['React', 'Node.js', 'PostgreSQL', 'AWS'],
+        workDescription: [
+          'Developing scalable full-stack features with React, Node.js, and PostgreSQL in a cloud-native environment.',
+          'Integrated third-party APIs and handled S3 log management with hourly rotations.',
+          'Introduced asynchronous activity logging and optimized app performance.',
+          'Led code reviews and mentored new hires to ensure code quality and process adherence.',
+        ],
+      },
+      {
+        position: 'Co-op Software Developer',
+        jobType: 'Internship',
+        timeline: 'Jan 2025 - Apr 2025',
+        skills: ['React', 'Node.js', 'PostgreSQL', 'AWS'],
+        workDescription: [
+          'Developing scalable full-stack features with React, Node.js, and PostgreSQL in a cloud-native environment.',
+          'Integrated third-party APIs and handled S3 log management with hourly rotations.',
+          'Introduced asynchronous activity logging and optimized app performance.',
+          'Led code reviews and mentored new hires to ensure code quality and process adherence.',
+        ],
+      },
     ],
   },
   {
-    company: 'Beacontree Technologies',
-    companyWebsite: 'https://beacontree.ca/',
-    companyLogo:
-      'https://beacontree.net/wp-content/uploads/2022/02/beacontree-technologies.png',
-    position: 'Software Engineer (Co-op)',
-    jobType: 'Internship',
-    jobLocation: 'Canada, Remote',
-    timeline: 'Feb 2025 - Apr 2025',
-    workDescription: [
-      'Worked on full-stack solutions using React, Node.js, and Android.',
-      'Built and maintained key modules for operational efficiency.',
-      'Participated in sprint planning and Agile development cycles.',
-      'Contributed to design discussions and implemented scalable features.',
-    ],
-  },
-  {
-    company: 'Dunn Edwards Paints',
+    company: 'Software Q',
     companyWebsite: 'https://www.dunnedwards.com/',
     companyLogo:
-      'https://yt3.googleusercontent.com/PRhpY1fH8UbQRkna5h1MbnichXFBue_DMunJFfbXOrzgMeGjhf1LJrhu7QzFwtXuLlGvDTWs=s900-c-k-c0x00ffffff-no-rj',
-    position: 'Technical Consultant',
-    jobType: 'Contract',
-    jobLocation: 'United States , Remote',
-    timeline: 'Feb 2024 - Aug 2024',
-    workDescription: [
-      'Built .NET Background Service to sync HubSpot and SAP internal Database.',
-      'Stabilized Incentive admin application and deployed via AWS across all environments.',
-      'Maintained Shopify and .NET internal worker services.',
+      'https://cdn.prod.website-files.com/675e75c50d1a187fc431553e/675e7841b78063f21e999138_owl-p-500.png',
+    jobLocation: 'Malaysia, Remote',
+    positions: [
+      {
+        position: 'Senior Software Engineer',
+        jobType: 'Contract',
+        timeline: 'Mar 2024 - Aug 2024',
+        workDescription: [
+          'Designed a commercial property management platform from scratch.',
+          'Enhanced Kiosk systems using .NET and Angular with real-time payment integration.',
+          'Worked in a globally distributed Agile team, streamlining daily deliverables.',
+        ],
+      },
     ],
   },
   {
-    company: 'Resimator',
+    company: 'Dunn-Edwards Paints (via Upwork)',
+    companyWebsite: 'https://www.dunnedwards.com/',
+    companyLogo:
+      'https://media.licdn.com/dms/image/v2/C4D0BAQFA4pxszwAehA/company-logo_200_200/company-logo_200_200/0/1631350702459?e=1750896000&v=beta&t=VEff4w2iejKdCiV5hH_NxHb9kRgKdn6z3oL15Bz3UAU',
+    jobLocation: 'United States , Remote',
+    positions: [
+      {
+        position: 'Technical Consultant',
+        jobType: 'Contract',
+        timeline: 'Feb 2024 - Aug 2024',
+        workDescription: [
+          'Designed and delivered scalable web applications for Dunn-Edwards Paints.',
+          'Collaborated with cross-functional teams and advised on system architecture.',
+        ],
+      },
+    ],
+  },
+  {
+    company: 'Resimator Oy',
     companyWebsite: 'https://resimator.fi/en',
     companyLogo: '../../../../assets/images/resimator_logo.jpeg',
-    position: 'Senior Software Engineer',
-    jobType: 'Full Time',
     jobLocation: 'Finland , Remote',
-    timeline: 'Nov 2022 - Feb 2024',
-    workDescription: [
-      'Senior Software Engineer at Izy, specializing in Angular, .NET, and C# development.',
-      'Led web application enhancements for improved user experiences at Izy.',
-      'Proficient in .NET for scalable solutions and seamless feature integration.',
-      'Daily international team coordination at Izy for cross-border collaboration.',
+    positions: [
+      {
+        position: 'Senior Software Engineer',
+        jobType: 'Full Time',
+        timeline: 'Nov 2022 - Feb 2024',
+        workDescription: [
+          'Delivered full-stack business solutions tailored for real estate.',
+          'Upgraded UI/UX with Angular and improved backend scalability using .NET.',
+          'Provided architectural recommendations and contributed to product growth strategies.',
+        ],
+      },
     ],
   },
   {
-    company: 'Verisk',
-    position: 'Software Engineer',
-    timeline: 'Aug 2021 - Aug 2022',
-    workDescription: [
-      'Constructed full-stack software: .NET MVC, Angular, React, and Node.js.',
-      'Gained AWS experience in cloud infrastructure and deployment.',
-      'Collaborated in Agile methodologies at a multinational company.',
-      'Enhanced communication and leadership via mentoring trainees.',
+    company: 'Verisk ISO',
+    companyWebsite: 'https://www.verisk.com/',
+    companyLogo:
+      'https://media.licdn.com/dms/image/v2/C4E0BAQHsjp1GOw4Rmw/company-logo_200_200/company-logo_200_200/0/1641304478140/verisk_analytics_logo?e=1750896000&v=beta&t=Y31EprZBJrkxUM_juvBq8fT5xWRwSCq2uyZ-HbHUa-Q',
+    jobLocation: 'Kathmandu Nepal , Hybrid',
+    positions: [
+      {
+        position: 'Software Engineer',
+        timeline: 'Aug 2021 - Aug 2022',
+        jobType: 'Full Time',
+        workDescription: [
+          'Migrated a central claims processing system to .NET & Angular, improving performance.',
+          'Built internal admin tools that significantly reduced user support time.',
+          'Mentored junior team members and promoted best practices in DevOps workflows.',
+        ],
+      },
     ],
   },
   {
-    company: 'Crossover',
-    companySlogan: 'Ultimate solutions to maintain hospitality',
-    project: 'Pms',
-    projectUrl: 'http://sales.crossoverpms.com/',
-    position: 'Junior Software Engineer',
-    timeline: 'Dec 2019 - Aug 2021',
-    workDescription: [
-      'Started as Intern Software Developer, learned diverse programming.',
-      'Promoted to Engineer, led Full-Stack projects for top Nepalese hotels.',
-      'Mastered development phases, demonstrating expertise in software engineering.',
-      'Progressed from intern to engineer, showcasing leadership in full-stack projects.',
+    company: 'Crossover Nepal',
+    companyWebsite: 'https://www.crossovernepal.com/',
+    companyLogo: 'https://www.crossovernepal.com/images/crossover.jpg',
+    jobLocation: 'Kathmandu Nepal , Physical',
+    positions: [
+      {
+        position: 'Junior Software Engineer',
+        timeline: 'Apr 2020 – Aug 2021',
+        jobType: 'Full Time',
+        workDescription: [
+          'Developed modules for a property management platform and appointment booking apps.',
+          'Gained full-stack experience in .NET, Angular, and Ionic.',
+          'Promoted from intern to team lead on hospitality projects.',
+        ],
+      },
+      {
+        position: 'Intern Software Engineer',
+        timeline: 'Dec 2019 – Apr 2020',
+        jobType: 'Internship',
+        workDescription: [
+          'Developed modules for a property management platform and appointment booking apps.',
+          'Gained full-stack experience in .NET, Angular, and Ionic.',
+          'Promoted from intern to team lead on hospitality projects.',
+        ],
+      },
     ],
   },
 ];
